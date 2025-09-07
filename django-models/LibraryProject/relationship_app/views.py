a80eb30b653f4e8d83d0905688d4d1642ee24b5c
momo <monimsrw@outlook.fr>
2025-09-07 03:55:15 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book
-from .models import  Library
-
+from .models import Library
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
