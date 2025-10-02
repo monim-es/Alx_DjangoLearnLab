7cd9e9811621788577c77cbe728de84ad8e62906
momo <monimsrw@outlook.fr>
2025-10-02 23:56:40 +0100

    2. Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,16 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    # Auth
     path("register/", views.register_view, name="register"),
     path("profile/", views.profile_view, name="profile"),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
+
+    # Blog Posts CRUD
+    path("posts/", views.PostListView.as_view(), name="post-list"),
+    path("post/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
 ]
